86978d9b3c8f9dfbe08bdbb8cee3759383f6505d
Ekaitz <al.ekaitz@gmail.com>
2022-02-07 00:54:47 +0100

    Cleanup --> index.js

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-// Connect to the database
+// Connect to database
 mongoose
-  .connect(process.env.MONGO_DB_URI, { useNewUrlParser: true })
-  .then(() => console.log(`Database connected successfully`))
-  .catch((err) => console.log(err));
+    .connect(process.env.MONGO_DB_URI, { useNewUrlParser: true })
+    .then(() => console.log(`Database connected successfully`))
+    .catch((err) => console.log(err));
 
-// Since mongoose's Promise is deprecated, we override it with Node's Promise
+// Mongoose's promise is deprecated -> Override with node's promise
 mongoose.Promise = global.Promise;
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
 });
 
 app.use(bodyParser.json());
@@ -28,10 +28,8 @@ app.use(bodyParser.json());
 app.use('/api', routes);
 
 app.use((err, req, res, next) => {
-  console.log(err);
-  next();
+    console.log(err);
+    next();
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.listen(port, () => console.log(`Server running on port ${port}`));
