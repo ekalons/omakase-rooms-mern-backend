84f9573acd5f7504bf51fde1022df3913a745ee1
Ekaitz <66970241+ekalons@users.noreply.github.com>
2024-09-02 19:26:05 +0200

    Remove post & delete endpoints (#6)

--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Room = require('../models/room');
+const Room = require("../models/room");
 
-router.get('/rooms', (req, res, next) => {
-    Room.find({})
-    .then((data) => res.json(data))
-    .catch(next);
-});
-
-router.post('/rooms', (req, res, next) => {
-    if (req.body.name) {
-        Room.create(req.body)
-        .then((data) => res.json(data))
-        .catch(next);
-    } else {
-    res.json({
-        error: 'The input field is empty',
-    });
-    }
-});
-
-router.delete('/rooms/:id', (req, res, next) => {
-    Room.findOneAndDelete({ _id: req.params.id })
+router.get("/rooms", (req, res, next) => {
+  Room.find({})
     .then((data) => res.json(data))
     .catch(next);
 });
 
 module.exports = router;
-
