802817a506abde553cc5488654602b760ab2faf7
Ekaitz <66970241+ekalons@users.noreply.github.com>
2025-07-19 14:22:10 +0200

    Bump node to v22 (#7)
    * Bump node to v22
    
    * Apply linting

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const routes = require('./routes/api');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes/api");
+require("dotenv").config();
 
 const app = express();
 
@@ -10,26 +10,29 @@ const port = process.env.PORT || 8080;
 
 // Connect to database
 mongoose
-    .connect(process.env.MONGO_DB_URI, { useNewUrlParser: true })
-    .then(() => console.log(`Database connected successfully`))
-    .catch((err) => console.log(err));
+  .connect(process.env.MONGO_DB_URI, { useNewUrlParser: true })
+  .then(() => console.log(`Database connected successfully`))
+  .catch((err) => console.log(err));
 
 // Mongoose's promise is deprecated -> Override with node's promise
 mongoose.Promise = global.Promise;
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 app.use(bodyParser.json());
 
-app.use('/api', routes);
+app.use("/api", routes);
 
 app.use((err, req, res, next) => {
-    console.log(err);
-    next();
+  console.log(err);
+  next();
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
